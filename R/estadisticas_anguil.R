a72989425870ceb0b2c6e259a318480f96c3fef0
JUAN PABLO FERRANDEZ <jferrandez@policiafederal.gov.ar>
2022-04-28 16:43:42 -0300

    Prueba para estadisticas datos anguil #12

--- R/estadisticas_anguil.R
+++ R/estadisticas_anguil.R
@@ -3,7 +3,58 @@
 estadisticasAnguilUI <- function(id) {
   ns <- NS(id)
   
-  tagList(tags$h1("Estadisticas Anguil"))
+  tagList(
+    tags$h1("Estadisticas Anguil"),
+    
+    tags$h2("2022"),
+    fluidRow(lapply(1:12, function(i) {
+      actionButton(
+        paste0('boton', mes_id[i]),
+        label = mes_desc[i],
+        value = mes_id[i],
+        onclick = paste0(
+          "window.open('estadisticas_anguil/Sintesis_",
+          mes_id[i] ,
+          "2022" ,
+          ".xlsx','_blank','resizable')"
+        )
+      )
+    })),
+    
+    tags$h2("2021"),
+    fluidRow(lapply(1:12, function(i) {
+      actionButton(
+        paste0('boton', mes_id[i]),
+        label = mes_desc[i],
+        value = mes_id[i],
+        onclick = paste0(
+          "window.open('estadisticas_anguil/Sintesis_",
+          mes_id[i] ,
+          "2021" ,
+          ".xlsx','_blank','resizable')"
+        )
+      )
+    })),
+    
+    tags$h2("2020"),
+    fluidRow(lapply(1:12, function(i) {
+      #column(1,
+      actionButton(
+        paste0('boton', mes_id[i]),
+        label = mes_desc[i],
+        value = mes_id[i],
+        onclick = paste0(
+          "window.open('estadisticas_anguil/Sintesis_",
+          mes_id[i] ,
+          "2020" ,
+          ".xlsx','_blank','resizable')"
+        )
+      )
+      #)
+    }))
+    
+    
+  )
 }
 
 #################################
@@ -11,6 +62,7 @@ estadisticasAnguilUI <- function(id) {
 estadisticasAnguilServer <- function(id) {
   moduleServer(id,
                function(input, output, session) {
+                 ns <- session$ns
                  
                })
 }
