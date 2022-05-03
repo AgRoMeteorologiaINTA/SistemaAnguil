595e660618b74c09d1e284bd1a829fe789555a36
jp <jpferrandez@gmail.com>
2022-05-03 18:19:30 -0300

    Close issue #13

--- R/links.R
+++ R/links.R
@@ -5,12 +5,34 @@ linksUI <- function(id) {
   
   tagList(
     tags$h1("Links"),
-    tags$h4(
-      "link donde los usuarios puedan acceder a la información que muestran estas estaciones automáticas (que se actualizan cada 10 minutos)."
-    ),
-    tags$a("Pagina INTA - Anguil", href = "https://inta.gob.ar/anguil", target =
-             "_blank")
+    tags$a("INTA EEA Anguil", 
+           href = "https://inta.gob.ar/anguil", target = "_blank"),
+    tags$br(),
+    tags$a("INTA Instituto de Clima y Agua", 
+           href = "https://inta.gob.ar/clima-y-agua", target = "_blank"),
+    tags$br(),
+    tags$a("INTA Sistema de Información y Gestión Agrometeorológica", 
+           href = "http://siga.inta.gob.ar/#/", target = "_blank"),
+    tags$br(),
+    tags$a("INTA Geointa", 
+           href = "http://www.geointa.inta.gob.ar", target = "_blank"),
+    tags$br(),
+    tags$a("Radares Meteorológicos de INTA", 
+           href = "https://radar.inta.gob.ar", target = "_blank"),
+    tags$br(),
+    tags$a("INTA ICyA Herramientas satelitales para el seguimiento de la producción agropecuaria", 
+           href = "http://sepa.inta.gob.ar", target = "_blank"),
+    tags$br(),
+    tags$a("INTA Pronósticos a corto plazo", 
+           href = "https://inta.gob.ar/documentos/pronosticos", target = "_blank"),
+    tags$br(),
+    tags$a("Servicio Meteorológico Nacional", 
+           href = "https://www.smn.gob.ar", target = "_blank"),
+    tags$br(),
+    tags$a("Gob. de La Pampa Red de Estaciones Climáticas", 
+           href = "https://lapampa.redesclimaticas.com/accounts", target = "_blank")
   )
+  
 }
 
 #################################
